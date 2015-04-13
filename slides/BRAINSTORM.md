41e47892e213046b5e93f3b04dc6de4b223f5c28
Jean-Marc MEESSEN <jean-marc@meessen-web.org>
2015-04-13 16:16:45 +0200

    Added Jmm's ideas to the slides

--- slides/BRAINSTORM.md
+++ slides/BRAINSTORM.md
@@ -77,3 +77,67 @@ Que disent les pratiques d'ingénieurie logicielle (plus ou moins) modernes ?
 * qu'ai-je appris ?
 
 ## Jean-Marc
+
+### Why ?
+
+* seen a lot in nearly 30 years in IT
+  * victories / failures
+  * great peoples
+* still as enthousiastic to learn/share
+  * great stuff out there (admirative)
+  * make people's life easier
+  * I want to share why I am proud of what I do
+  * hope to be able to continue even after retirement
+* BUT, one of my big frustration
+  * Wasted (unfocused energy) at the start of a every new adventure
+
+### What is the problem ?
+
+
+* Wasted High power moment (high energy)
+* Setup of a work environement takes lot of time (comme une sortie de piste, dans les graviers)
+* perte du focus
+* Necessite d'avoir l'environement de DEV le plus riche possible (state of the art). Pas de nivellement par le bas.
+* très rapide à déployer et du premier coup
+* et je ne parle pas de l'env d'integration
+
+
+### Solutions
+
+* j'ai connu
+  * les terminaux sur le mainframe
+  * les PC avec images (quid des utilisateurs nomades)
+  * virtualisation avec Vagrant/VB par exemple
+* Docker
+  * rend des choses qu'on fesait depuis longtemps tres bon marche
+    * utilisation du GPS
+    * nouveau produits/nouvelles utilisations
+* utiliser (naturellement) les recettes de l'ingénierie logicielle 
+
+### Cristalisation
+
+* je rflechi beaucoup sur comment ameliorer notre travail au sein de WL
+* rencontre avec de jeunes collègues qui partagent ma vision de notre métier et de mes valeurs.
+  * plus aventureux que moi
+  * d'autres chemins d'expérience que moi
+* (Autant j'aime mes habitudes/certitudes), j'ai l'intuition que cette technologie pourraient nous aider
+  * l'idee est novatrice et va a l'encontre de mes habitudes (c'est tjr tres interessant)
+  * l'occasion d'apprendre cette nouvelle techno avec quelqu'un qui trempe dedans depuis plusieurs années.
+  * me confronter à d'autres manieres de travailler
+
+### ce que j'ai appris
+
+* j'ai appris de nouvelles technos (amélioré ma pratique)
+  * Docker
+  * Test Driven SYSTEM development (DEVOPS) 
+  * Bash and Linux skills
+  * the Gitlab/Github way
+  * confirmation que de la conception à la production, le flux est continu (mort aux silos)
+  * apprentissage du travail communautaire/ OSS (des valeurs qui me sont très proches)
+
+* Notre intuition est correcte:
+  * Encore rien rencontré de bloquant et très, très prometteur.
+  * Il y a encore du travail
+  * j'attend avec impatience l'épreuve du feu (situation réelle)
+
+
---
+++ slides/Jmm's notes on DEVBOX.mm
@@ -0,0 +1,149 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Jmm&apos;s notes&#xa;on DEVBOX" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1428927967720"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" BACKGROUND_COLOR="#cccccc" STYLE="as_parent">
+<font SIZE="18" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff" STYLE="bubble">
+<font SIZE="16" BOLD="true"/>
+<edge STYLE="sharp_bezier" WIDTH="4"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439" STYLE="combined" MAX_WIDTH="400">
+<font SIZE="14"/>
+<edge STYLE="sharp_bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000" STYLE="fork" MAX_WIDTH="500">
+<font SIZE="12"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111" STYLE="fork" MAX_WIDTH="600">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="11"/>
+<edge STYLE="bezier"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="Why" POSITION="right" ID="ID_702557640" CREATED="1428927809729" MODIFIED="1428927823171">
+<edge COLOR="#7c0000"/>
+<node TEXT="seen a lot in nearly 30 years in IT" ID="ID_1412547451" CREATED="1428927825124" MODIFIED="1428927852719">
+<node TEXT="lot of" ID="ID_1889773968" CREATED="1428927872059" MODIFIED="1428927877431">
+<node TEXT="victories" ID="ID_341023617" CREATED="1428927879053" MODIFIED="1428927882327"/>
+<node TEXT="failures" ID="ID_1962918570" CREATED="1428927882893" MODIFIED="1428927887583"/>
+<node TEXT="great peoples" ID="ID_412649176" CREATED="1428927890962" MODIFIED="1428927902470"/>
+</node>
+</node>
+<node TEXT="still as enthousiastic to learn/share" ID="ID_96308677" CREATED="1428928003337" MODIFIED="1428928066580">
+<node TEXT="great stuff out there (admirative)" ID="ID_724825840" CREATED="1428928032099" MODIFIED="1428928052052"/>
+<node TEXT="make people&apos;s life easier" ID="ID_717620777" CREATED="1428928052953" MODIFIED="1428928225016"/>
+<node TEXT="hope to be able to continue even after retirement" ID="ID_1119909909" CREATED="1428928302061" MODIFIED="1428928320743"/>
+</node>
+<node TEXT="BUT, one of my big frustration" ID="ID_1055059345" CREATED="1428928327439" MODIFIED="1428928418550">
+<node TEXT="Wasted (unfocused energy) at the start of a new adventure" ID="ID_4170443" CREATED="1428928488500" MODIFIED="1428928673671">
+<node TEXT="new project, new team" ID="ID_306246300" CREATED="1428928513279" MODIFIED="1428928527195"/>
+<node TEXT="a new consultant/prestataire" ID="ID_814309266" CREATED="1428928528336" MODIFIED="1428928542563"/>
+<node TEXT="a young trainee" ID="ID_1399214940" CREATED="1428928549922" MODIFIED="1428928567018">
+<node TEXT="I want to share why I am proud of what I do" ID="ID_15286290" CREATED="1428928572520" MODIFIED="1428928592193"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="what is the problem ?" POSITION="left" ID="ID_812694149" CREATED="1428928694060" MODIFIED="1428928706205">
+<edge COLOR="#00007c"/>
+<node TEXT="High power moment (high energy)" ID="ID_1134580809" CREATED="1428928715022" MODIFIED="1428928967942"/>
+<node TEXT="Setup of a work environement takes lot of time" ID="ID_1026940624" CREATED="1428928969805" MODIFIED="1428929002353">
+<node TEXT="sortie de piste, dans les graviers" ID="ID_1415939463" CREATED="1428929005185" MODIFIED="1428929020789"/>
+<node TEXT="si authorisation encore plus de temps" ID="ID_234648431" CREATED="1428929044440" MODIFIED="1428929055531">
+<node TEXT="environement securise" ID="ID_246902067" CREATED="1428929140572" MODIFIED="1428929147913"/>
+</node>
+<node TEXT="perte du focus" ID="ID_1285124640" CREATED="1428929061586" MODIFIED="1428929156069"/>
+<node TEXT="mais de l&apos;autre" ID="ID_637988499" CREATED="1428929118672" MODIFIED="1428929138147"/>
+<node TEXT="environement le plus riche possible (state of the art)" ID="ID_1660746152" CREATED="1428929167618" MODIFIED="1428929197278"/>
+<node TEXT="aligne au niveau de l&apos;equipe" ID="ID_1981937484" CREATED="1428929208572" MODIFIED="1428929219870">
+<node TEXT="! one size fits all" ID="ID_1112870731" CREATED="1428929258596" MODIFIED="1428929278944"/>
+</node>
+<node TEXT="tres rapide a deployer" ID="ID_112720940" CREATED="1428929313349" MODIFIED="1428929324750"/>
+<node TEXT="et je ne parle pas de l&apos;env d&apos;integration" ID="ID_663567556" CREATED="1428929423873" MODIFIED="1428929448872"/>
+<node TEXT="j&apos;ai connu" ID="ID_800313912" CREATED="1428929341119" MODIFIED="1428929348671">
+<node TEXT="les terminaux sur le mainframe" ID="ID_150464082" CREATED="1428929349663" MODIFIED="1428929378654"/>
+<node TEXT="les PC avec images" ID="ID_1062614483" CREATED="1428929379623" MODIFIED="1428929398338">
+<node TEXT="? utilisateurs nomades" ID="ID_603492436" CREATED="1428929399995" MODIFIED="1428929413722"/>
+</node>
+<node TEXT="virtualisation avec Vagrant/VB par exemple" ID="ID_585409390" CREATED="1428929497725" MODIFIED="1428929537614"/>
+</node>
+<node TEXT="Docker" ID="ID_1167157105" CREATED="1428929476595" MODIFIED="1428929496833">
+<node TEXT="rend des choses qu&apos;on fesait depuis longtemps tres bon marche" ID="ID_65319032" CREATED="1428929551683" MODIFIED="1428929584029">
+<node TEXT="utilisation du GPS" ID="ID_216971917" CREATED="1428929592857" MODIFIED="1428929601013"/>
+<node TEXT="nouveau produits/nouvelles utilisations" ID="ID_1802157176" CREATED="1428929605517" MODIFIED="1428929633002"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="mais aussi une philosophie" POSITION="right" ID="ID_1560154875" CREATED="1428929661019" MODIFIED="1428929722999">
+<edge COLOR="#007c00"/>
+<node TEXT="technique de developement" ID="ID_1044092253" CREATED="1428929752780" MODIFIED="1428929762438"/>
+</node>
+<node TEXT="pourquoi moi" POSITION="left" ID="ID_625773182" CREATED="1428929813858" MODIFIED="1428929822370">
+<edge COLOR="#7c007c"/>
+<node TEXT="je reflechi sur comment ameliorer notre travail" ID="ID_1406552654" CREATED="1428929824378" MODIFIED="1428929846555"/>
+<node TEXT="Autant j&apos;aime mes habitudes (certitudes)" ID="ID_1177994360" CREATED="1428929847991" MODIFIED="1428929875418">
+<node TEXT="il y a des choses la qui pourraient nous aider" ID="ID_1983393533" CREATED="1428929877697" MODIFIED="1428929902203"/>
+<node TEXT="l&apos;idee est novatrice et va a l&apos;encontre de mes habitudes" ID="ID_954483131" CREATED="1428929915893" MODIFIED="1428929947585"/>
+<node TEXT="mais j&apos;ai l&apos;intuition qu&apos;il  y a de la musique la dedans" ID="ID_1244990862" CREATED="1428929952233" MODIFIED="1428929986840"/>
+<node TEXT="l&apos;occasion d&apos;apprendre cette nouvelle techno" ID="ID_1298298852" CREATED="1428929988814" MODIFIED="1428930013905">
+<node TEXT="se remettre en question" ID="ID_950813627" CREATED="1428930016126" MODIFIED="1428930022568"/>
+</node>
+<node TEXT="apprendre une nouvelle maniere de travailler" ID="ID_631194031" CREATED="1428930053666" MODIFIED="1428930083152"/>
+</node>
+</node>
+<node TEXT="ce que j&apos;ai appris" POSITION="right" ID="ID_189049155" CREATED="1428930129684" MODIFIED="1428930139491">
+<edge COLOR="#007c7c"/>
+<node TEXT="apprendre de nouvelles technos" ID="ID_1606869643" CREATED="1428930165019" MODIFIED="1428930202535">
+<node TEXT="Docker" ID="ID_438197902" CREATED="1428930203684" MODIFIED="1428930207438"/>
+<node TEXT="Test Driven SYSTEM development" ID="ID_1677660799" CREATED="1428930209998" MODIFIED="1428930239669">
+<node TEXT="DEVOPS" ID="ID_1607844735" CREATED="1428930240601" MODIFIED="1428930244251"/>
+</node>
+<node TEXT="Bash and Linux skills" ID="ID_899462854" CREATED="1428930259009" MODIFIED="1428930280808"/>
+<node TEXT="Gitlab" ID="ID_242520019" CREATED="1428930288107" MODIFIED="1428930296721"/>
+</node>
+<node TEXT="excellent outil de formation a la methodologie" ID="ID_209513013" CREATED="1428930302352" MODIFIED="1428930430043"/>
+<node TEXT="confirmation de la conception a la production" ID="ID_348199164" CREATED="1428930314721" MODIFIED="1428930411967">
+<node TEXT="un flux continu" ID="ID_540673841" CREATED="1428930344671" MODIFIED="1428930373067"/>
+</node>
+<node TEXT="apprentissage du travail communautaire/ OSS" ID="ID_1912856068" CREATED="1428930439379" MODIFIED="1428930473621">
+<node TEXT="des valeurs qui me sont tres proches" ID="ID_818024180" CREATED="1428930474300" MODIFIED="1428930486998"/>
+</node>
+</node>
+</node>
+</map>
